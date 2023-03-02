9ab95224e633d06aa4313be57bf5114877069b4e
xellanox <djones94@protonmail.com>
2023-03-02 08:04:59 +0000

    Gatekeeper context DI to gatekeeper collection

--- FulcrumServer/Program.cs
+++ FulcrumServer/Program.cs
@@ -7,10 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<GatekeeperContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("default"), b => b.MigrationsAssembly("libGatekeeper")));
-
-builder.Services.AddGatekeeper();
+builder.Services.AddGatekeeper(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- libGatekeeper/GatekeeperServiceGroup.cs
+++ libGatekeeper/GatekeeperServiceGroup.cs
@@ -1,14 +1,19 @@
 using System.Globalization;
 using libGatekeeper.Services.Authentication;
 using libGatekeeper.Services.UserManagement;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using libGatekeeper;
+using Microsoft.EntityFrameworkCore;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class GatekeeperServiceGroup
 {
-    public static IServiceCollection AddGatekeeper(this IServiceCollection services)
+    public static IServiceCollection AddGatekeeper(this IServiceCollection services, IConfiguration Configuration)
     {
+        services.AddDbContext<GatekeeperContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("default"), b => b.MigrationsAssembly("libGatekeeper")));
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<IUserManagementService, UserManagementService>();
 
