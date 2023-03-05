ceb8093b1610250691e002f58055c7ecdc7cabe0
xellanox <djones94@protonmail.com>
2023-03-05 19:17:06 +0000

    regen migrations

--- Migrations/20230305191652_AddChanges.Designer.cs
+++ Migrations/20230305191652_AddChanges.Designer.cs
@@ -4,6 +4,7 @@ using Fulcrum;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fulcrum.Migrations
 {
     [DbContext(typeof(FulcrumContext))]
-    partial class FulcrumContextModelSnapshot : ModelSnapshot
+    [Migration("20230305191652_AddChanges")]
+    partial class AddChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230305191652_AddChanges.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fulcrum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ArtFilename",
+                table: "Mediafiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArtPath",
+                table: "Mediafiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MimeType",
+                table: "Mediafiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArtFilename",
+                table: "Mediafiles");
+
+            migrationBuilder.DropColumn(
+                name: "ArtPath",
+                table: "Mediafiles");
+
+            migrationBuilder.DropColumn(
+                name: "MimeType",
+                table: "Mediafiles");
+        }
+    }
+}
